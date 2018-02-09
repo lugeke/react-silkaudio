6495adbb27ee1f07398286c1fc3a3f3844b17ca1
jiaheng <lugeke@gmail.com>
2018-02-10 00:15:43 +0800

    add yarn build

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IP="138.68.178.137"
-# yarn build && 
+yarn build && 
 tar czf build.tar.gz ./build && ssh root@$IP "mkdir -p /data/www/silkaudio/static " &&
 scp build.tar.gz root@$IP:/data/www/silkaudio &&
 ssh root@$IP "cd /data/www/silkaudio && rm -rf static/js/* static/css/* && tar -xzf build.tar.gz  --strip-components=2 -C . && rm -f build.tar.gz" 
